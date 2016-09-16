e2e32f77b220e51e8eabd0744c74027156d0e12c
Achintya Ashok <aa3099@nyu.edu>
2016-09-16 00:54:23 -0400

    added in parts to calculate similarity score and to generate the index for a word

--- play.js
+++ play.js
@@ -1,1 +1,31 @@
 console.log("Starting mastermind game.");
+
+var corpus = [ "jello", "world", "black", "green", "hello", "champ", "hound"];
+
+/* This function generates an index for a word by comparing it to all
+other words in the word list. It then maps each word pair to the comparison score.
+It will return a score list for the input word. This score list will contain
+all words that have 0..n letters in common with the key word (n is also the index
+in the score list). Ex. @ index 2, all words have 2 letters 'in common' with the key */
+function generateIndexForWord(word, wordList){
+  var scoreList = [];
+  for(var i=0; i<=word.length; ++i) scoreList.push([]);
+  for(var i=0; i<wordList.length; ++i){
+    var compareWord = wordList[i];
+    if(word === compareWord) continue;
+    var comparisonScore = calculateSimilarityScore(word, compareWord);
+    scoreList[comparisonScore].push(compareWord); // keep track of the score this word pertains to
+  }
+}
+
+/* Returns a number indicating the number of common letters between the
+first and second parameters. In order for a letter to be common, it must
+in the same place in both strings. */
+function calculateSimilarityScore(first, second){
+  if(first.length != second.length) throw("inputs are of different lengths");
+  var score = 0;
+  for(var i=0; i<first.length; i++){
+    if(first[i] === second[i]) ++score;
+  }
+  return score;
+}
